330777fe4b2683fa68c47c87e58fbe91e03777c9
Harsh Sharma <harshsharma301a@gmail.com>
2025-02-10 20:56:21 +0530

    Added Controllers and Routes for Creating Tweet

---
+++ src/controller/tweet-controller.js
@@ -0,0 +1,23 @@
+import { tweetService } from "../services/tweetservice.js";
+
+const tweetservice = new tweetService()
+
+export const createTweet = async (req , res) =>{
+    try {
+        const tweet = await tweetservice.create(req.body)
+        return res.status(201).json({
+            success : true,
+            err : {},
+            data : tweet,
+            message : "Succesfully Created a new Tweet"
+
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success : false,
+            err : error ,
+            data : {},
+            message : "Cannot create a tweet"
+        })
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import express from 'express'
 import {connect} from './config/database.js'
 // const { PORT } = require('./config/serverconfig')
 import { PORT } from './config/serverconfig.js'
+import bodyparser from 'body-parser'
 // const connect = require('./config/database')
 const app = express()
+
+import apiRoutes from './routes/index.js'
 
 
 // const Tweet = require('./models/tweet')
@@ -17,12 +20,15 @@ import { tweetService } from './services/tweetservice.js'
 
 app.listen(PORT, async () => {
     console.log(`Server Started on PORT : ${PORT}`)
+    app.use(bodyparser.json())
+    app.use(bodyparser.urlencoded({extended : true}))
     await connect();
+    app.use('/api' , apiRoutes)
     console.log("Mongo DB Connected")
 
     const service = new tweetService()
     // await service.create({content : "This is #after #Refactoring"})
-    await service.create({content : "This is #NEW #CONtent with new #hashtags"})
+    // await service.create({content : "This is #NEW #CONtent with new #hashtags"})
     
    
 
